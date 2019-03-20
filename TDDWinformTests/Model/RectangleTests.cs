14b9ae337e5e8c32ca850a88346a81d255829562
Bijay Rai <b33j0y@live.com>
2019-03-20 20:11:07 +0545

    Unit test stub created

---
+++ TDDWinformTests/Model/RectangleTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDDWinform.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDWinform.Model.Tests
+{
+    [TestClass()]
+    public class RectangleTests
+    {
+        [TestMethod()]
+        public void CalculateAreaTest()
+        {
+            Rectangle rect = new Rectangle();
+            rect.Length = 12;
+            rect.Breadth = 3;
+
+            Assert.AreEqual(36, rect.CalculateArea());
+        }
+    }
+}
