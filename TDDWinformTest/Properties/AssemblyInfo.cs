d8e8873452437d4f24b635ac70275bed89229a6a
Bijay Rai <b33j0y@live.com>
2019-03-20 19:46:52 +0545

    Unit test project added

---
+++ TDDWinformTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TDDWinformTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TDDWinformTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d5f02e00-4088-4669-a183-18b3152c2d0d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TDDWinformTest/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDDWinform.Model;
+
+namespace TDDWinformTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CalculateAreaTest()
+        {
+            Rectangle rect = new Rectangle();
+            rect.Length = 12;
+            rect.Breadth = 3;
+
+            Assert.AreEqual(12, rect.Length);
+            Assert.AreEqual(3, rect.Breadth);
+            Assert.AreEqual(36, rect.CalculateArea());
+        }
+    }
+}
